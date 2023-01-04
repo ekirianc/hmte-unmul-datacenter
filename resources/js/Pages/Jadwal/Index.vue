5c75d4814b1a27dd090616ccbf560432d4c47ce0
HigerPhi <ekidoseven@gmail.com>
2023-01-04 23:26:25 +0800

    add jadwal page

---
+++ resources/js/Pages/Jadwal/Index.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <TabsWrapper>
+        <TabItem title="Kuliah" >
+            <Kuliah/>
+
+        </TabItem>
+        <TabItem title="Seminar" >
+            <div class="text-center">Ini jadwal Kuliah</div>
+        </TabItem>
+        <TabItem title="Organisasi" >
+            <div class="text-center">Ini jadwal Kuliah</div>
+        </TabItem>
+
+    </TabsWrapper>
+
+
+
+
+</template>
+
+<script>
+import Layout from '../../Layouts/App.vue'
+// Tabs
+import Wrapper from '../../Components/Tabs/Wrapper.vue'
+import Item from '../../Components/Tabs/Item.vue'
+// Section
+import Kuliah from "@/Pages/Jadwal/Kuliah.vue";
+
+export default {
+    layout: Layout,
+    components:{
+        TabsWrapper: Wrapper,
+        TabItem: Item,
+        Kuliah
+    },
+
+}
+</script>
+
+
+
